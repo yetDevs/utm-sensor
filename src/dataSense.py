4d967af5241b419c63f058f072cf1fee7879662b
Yetee <yetee.pay@gmail.com>
2023-11-28 18:53:48 -0500

    Updates

--- src/dataSense.py
+++ src/dataSense.py
@@ -1,23 +1,25 @@
 import argparse
 import requests
 import pandas as pd
+import json
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--req_url", type=str, required=True)
-    parser.add_argument("--req_data", type=str, required=True)
+    parser.add_argument("--req_data", type=argparse.FileType('r'), required=True)
     args = parser.parse_args()
     return args
 
-def get_response(req_url, req_data):
-    response = requests.post(req_url, data={'info': req_data})
+def get_response(req_data_file):
+    req_url = 'https://enqeqqyg157lb.x.pipedream.net' #Change this to your own URL
+    req_data = json.load(req_data_file)
+    response = requests.post(req_url, json=req_data)
     status = 'Fail' if response.status_code == 200 else 'Pass'
     df = pd.DataFrame({'URL': [req_url], 'Status Code': [response.status_code], 'Status': [status]})
     return df
 
 def main():
     args = parse_args()
-    df = get_response(args.req_url, args.req_data)
+    df = get_response(args.req_data)
     print(df)
 
 if __name__ == "__main__":
